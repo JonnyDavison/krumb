d023c6f495bc6e03435ad41c3960e9be2ff5261b
JonnyDavison <jonnydavison@icloud.com>
2024-04-30 09:17:39 +0000

    Add allowed hosts

--- krumb/settings.py
+++ krumb/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['krumb-96963189792b.herokuapp.com', '8000-jonnydavison-krumb-mnm5adbrqnd.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['www.krumbbakery.com', 'krumb-96963189792b.herokuapp.com', '8000-jonnydavison-krumb-mnm5adbrqnd.ws-eu110.gitpod.io']
 
 
 # Application definition
