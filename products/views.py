0b98797ad7ea3791297a48f32417a92d753d27e0
JonnyDavison <jonnydavison@icloud.com>
2024-05-14 15:07:22 +0000

    Create products app

---
+++ products/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
+# from .models import Product, Category
+# from .forms import ProductForm
+
+
+
+def all_products(request):
+    """
+    A view to return all Products page
+    """
+
+    products = Product.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_categories': categories
+    }
+
+    return render(request, 'products/products.html', context)
