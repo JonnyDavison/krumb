06a3cd31ff7c2764f4cfdca67f5b2c0e71fe4811
JonnyDavison <jonnydavison@icloud.com>
2024-05-27 19:49:58 +0000

    Create about & findu .html, add urls and views

--- index/views.py
+++ index/views.py
@@ -6,3 +6,16 @@ def index(request):
     A view to return the index page
     """
     return render(request, 'index/index.html')
+
+
+def findus(request):
+    """
+    A view to return the Fund Us page
+    """
+    return render(request, 'index/findus.html')
+
+def about(request):
+    """
+    A view to return the Fund Us page
+    """
+    return render(request, 'index/about.html')
--- krumb/urls.py
+++ krumb/urls.py
@@ -7,7 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    
     path('', include('index.urls')),
     path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
